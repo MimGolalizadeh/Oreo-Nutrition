436a4814fbdcd79a05e7f116d1674335e66585ef
Mojtaba Golalizadeh <106627627+MimGolalizadeh@users.noreply.github.com>
2022-06-04 22:57:37 +0200

    Update Oreo-Nutrition-Calculator.py

--- Oreo-Nutrition-Calculator.py
+++ Oreo-Nutrition-Calculator.py
@@ -1,12 +1,13 @@
-description = """\n\033[1;37mHello world)
+description = """\nHello world)
 This application helps you to calculate how much calories, sodium, carbohydrate, fat you have consumed.
-with eating each Oreo cookie by asking you some questions. Please answer the questions carefully to have correct answers.
+with eating each Oreo cookie by asking you some questions. Please answer the questions carefully to have 
+correct answers.
 At the End it gives you final information and some advices \n"""
 print(description)
 
 # Getting information from user
-name = input("\033[1;38mWhat is your name? ").title()
-number_of_cookie = int(input("\033[1;38mHow many cookies have you got " + name +"? "))
+name = input("What is your name? ").title()
+number_of_cookie = int(input("How many cookies have you got " + name +"? "))
 
 # Defining variables
 calories = int((number_of_cookie) * 160)
@@ -16,14 +17,18 @@ carbohydrate = int((number_of_cookie) * 25)
 fat = int((number_of_cookie) * 7)
 
 # Show the result
-consumption_calculator = f"\033[1;35m{'-' * 50} \n\033[1;33m{name} ,you have got {number_of_cookie} cookie(s) \nYour consumption details: \033[1;32m \nCalories:        {calories} \nSodium:          {sodium}mg \nCarbohydrate:    {carbohydrate}gr \nFat:             {fat}gr"
+consumption_calculator = f"{'-' * 50} \n{name} ,you have got {number_of_cookie} cookie(s) \n" \
+                         f"Your consumption details:  \nCalories:        {calories} \n" \
+                         f"Sodium:          {sodium}mg \nCarbohydrate:    {carbohydrate}gr \nFat:             {fat}gr"
 print(consumption_calculator)
-print("\033[1;35m-" * 50)
+print("-" * 50)
 
 # Show the Warning or message - two different conditions for less/more than 2000kcal consumption
 if calories < calories_limit:
-    message = f"\033[1;34mWell done {name}, you have got less than {calories_limit}kcal,\033[1;31m*** eating more than {calories_limit}kcal per day might be unhealthy."
+    message = f"Well done {name}, you have got less than {calories_limit}kcal,*** eating more" \
+              f" than {calories_limit}kcal per day might be unhealthy."
 else:
-    message = f"\033[1;31m*** Please notice that eating more than {calories_limit}kcal per day might be unhealthy. Your health condition is matter."
+    message = f"*** Please notice that eating more than {calories_limit}kcal per day might be unhealthy. " \
+              f"Your health condition is matter."
 
 print(message)
