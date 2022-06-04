408eb592f351be64cd309be0ae876e26de30b9db
Mojtaba Golalizadeh <106627627+MimGolalizadeh@users.noreply.github.com>
2022-06-04 11:20:48 +0200

    Update Oreo-Nutrition-Calculator.py

--- Oreo-Nutrition-Calculator.py
+++ Oreo-Nutrition-Calculator.py
@@ -1,22 +1,22 @@
 description = """\n\033[1;37mHello world)
 This application helps you to calculate how much calories, sodium, carbohydrate, fat you have consumed.
-with eating each Oreo Coockie by asking you some questions. Please answer the questions carefully to have correct answers.
+with eating each Oreo cookie by asking you some questions. Please answer the questions carefully to have correct answers.
 At the End it gives you final information and some advices \n"""
 print(description)
 
 # Getting information from user
 name = input("\033[1;38mWhat is your name? ").title()
-number_of_coockie = int(input("\033[1;38mHow many cookies have you got " + name +"? "))
+number_of_cookie = int(input("\033[1;38mHow many cookies have you got " + name +"? "))
 
 # Defining variables
-calories = int((number_of_coockie) * 160)
+calories = int((number_of_cookie) * 160)
 calories_limit = 2000
-sodium = int((number_of_coockie) * 190)
-carbohydrate = int((number_of_coockie) * 25)
-fat = int((number_of_coockie) * 7)
+sodium = int((number_of_cookie) * 190)
+carbohydrate = int((number_of_cookie) * 25)
+fat = int((number_of_cookie) * 7)
 
 # Show the result
-consumption_calculator = f"\033[1;35m{'-' * 50} \n\033[1;33m{name} ,you have got {number_of_coockie} coockie(s) \nYour consumption details: \033[1;32m \nCalories:        {calories} \nSodium:          {sodium}mg \nCarbohydrate:    {carbohydrate}gr \nFat:             {fat}gr"
+consumption_calculator = f"\033[1;35m{'-' * 50} \n\033[1;33m{name} ,you have got {number_of_cookie} cookie(s) \nYour consumption details: \033[1;32m \nCalories:        {calories} \nSodium:          {sodium}mg \nCarbohydrate:    {carbohydrate}gr \nFat:             {fat}gr"
 print(consumption_calculator)
 print("\033[1;35m-" * 50)
 
