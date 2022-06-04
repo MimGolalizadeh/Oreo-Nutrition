0ed7dacb210a80f639dbd7e5bc053861df1cff85
Mojtaba Golalizadeh <106627627+MimGolalizadeh@users.noreply.github.com>
2022-06-04 09:41:08 +0200

    Update Oreo-Nutrition-Calculator.py

--- Oreo-Nutrition-Calculator.py
+++ Oreo-Nutrition-Calculator.py
@@ -1,6 +1,6 @@
 discription = """\n\033[1;37mHello world)
 This application helps you to calculate how much calories, sodium, carbohydrate, fat you have consumed.
-with eating each Oreo Cookie by asking you some questions. Please answer the questions carefully to have correct answers.
+with eating each Oreo Coockie by asking you some questions. Please answer the questions carefully to have correct answers.
 At the End it gives you final information and some advices \n"""
 print(discription)
 
